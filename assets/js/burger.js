105a69a2e30c963f3c09a7acd2bfffb053547b3d
WhyNotSayYes <bolgarsky.iu@gmail.com>
2022-09-10 21:38:51 +0300

    first init

---
+++ assets/js/burger.js
@@ -0,0 +1,7 @@
+const burger = document.querySelector('.header__burger');
+const menu = document.querySelector('.header__menu');
+
+burger.addEventListener('click', function() {
+    burger.classList.toggle('active');
+    menu.classList.toggle('active');
+})
