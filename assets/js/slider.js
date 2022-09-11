5692a7b278d35a4574347771867762ed5d65f624
WhyNotSayYes <bolgarsky.iu@gmail.com>
2022-09-11 20:19:22 +0300

    11.09 20-20

---
+++ assets/js/slider.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.reviews__slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        initialSlide: 1,
+        arrows: true,
+        dots: true,
+        fade: true,
+        cssEase: 'linear',
+        // autoplay: true
+    });
+})
+
