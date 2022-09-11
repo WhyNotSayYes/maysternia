02a9d702def32226f9cabb5bee0e833b4f9d0bd0
WhyNotSayYes <bolgarsky.iu@gmail.com>
2022-09-11 15:00:17 +0300

    11.09 15-00

--- assets/js/burger.js
+++ assets/js/burger.js
@@ -1,7 +1,16 @@
 const burger = document.querySelector('.header__burger');
 const menu = document.querySelector('.header__menu');
+const menuLink = menu.querySelectorAll('.header__menu_link');
+const body = document.querySelector('body');
 
 burger.addEventListener('click', function() {
     burger.classList.toggle('active');
     menu.classList.toggle('active');
-})
+    body.classList.toggle('lock');
+});
+
+menu.addEventListener('click', function() {
+    burger.classList.remove('active');
+    menu.classList.remove('active');
+    body.classList.remove('lock');
+});
