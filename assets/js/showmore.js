657ba09d2fe583e0602837215145813f1e426bd0
WhyNotSayYes <bolgarsky.iu@gmail.com>
2022-09-14 19:45:30 +0300

    14.09 19-45

--- assets/js/showmore.js
+++ assets/js/showmore.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const nextOpen = (wrapper, button) => {
 
         const items = wrapper.querySelectorAll('.portfolio__item'),
-        len = items.length - 2,
+        len = items.length - 1,
         endBox = wrapper.querySelector('.nextstop'),
         index = [...items].indexOf(endBox) + limit;
 
