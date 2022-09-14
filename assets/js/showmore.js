af357fdcaaedeee8f1e45fe5d1aae8626568d721
WhyNotSayYes <bolgarsky.iu@gmail.com>
2022-09-14 15:48:35 +0300

    14.09 15-50

---
+++ assets/js/showmore.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const limit = 4;
+    const nextOpen = (wrapper, button) => {
+
+        const items = wrapper.querySelectorAll('.portfolio__item'),
+        len = items.length - 2,
+        endBox = wrapper.querySelector('.nextstop'),
+        index = [...items].indexOf(endBox) + limit;
+
+    if(endBox) endBox.classList.remove('nextstop');
+    if(index < len) items[index].classList.add('nextstop');
+    else button.remove();
+
+    };
+
+    document.querySelectorAll('.portfolio__items').forEach(wrapper => {
+        const button = wrapper.nextElementSibling;
+        button.addEventListener('click', nextOpen.bind(null, wrapper, button));
+        nextOpen(wrapper, button);
+    });
+})
