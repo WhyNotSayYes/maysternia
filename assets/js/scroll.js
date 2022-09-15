f4c7f017380376d35f4f2f0d042b7ada1bff9bbe
WhyNotSayYes <bolgarsky.iu@gmail.com>
2022-09-15 20:13:29 +0300

    15.09 20-15

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,14 +1,12 @@
-    $(document).ready(function(){
+$(document).ready(function(){
 
-        $("body").on("click","[data-scroll]", function (event) {
-            event.preventDefault();
-            var id  = $(this).attr('data-scroll'),
-                nav = $('.header').height(),
-                top = $(id).offset().top - nav;
-            $('body,html').animate({scrollTop: top}, 1500);
-        });
-    })
+    $("body").on("click","[data-scroll]", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('data-scroll'),
+            nav = $('.header').height(),
+            top = $(id).offset().top - nav;
+        $('body,html').animate({scrollTop: top}, 1500);
 
+    });
 
-
-
+});
